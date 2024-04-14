46e440d9389e1a55d2cca89cb8a49794eaa54f0c
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-13 22:42:49 -0700

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Guy Steinberg on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
