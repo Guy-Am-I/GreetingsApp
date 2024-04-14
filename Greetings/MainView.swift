d9542081859290dbde00f2cddd39a86507a75e75
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-14 09:50:09 -0700

    Added simple reusable TextView to app

--- Greetings/MainView.swift
+++ Greetings/MainView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            TextView(text: "Hello There", color: .green)
+            TextView(text: "Welcome to Swift Programming", color: .gray)
+            TextView(text: "Are you ready to", color: .yellow)
+            TextView(text: "Start exploring", color: .red)
+            TextView(text: "Boom!", color: .purple)
+            
         }
+    }
+}
+
+struct TextView: View {
+    let text: String
+    let color: Color
+    
+    var body: some View {
+        Text(text)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .foregroundStyle(Color.white)
+            .padding()
+            .background(color.opacity(0.8))
+            .clipShape(Capsule())
+            .shadow(color: color.opacity(0.4), radius: 5, x: 10, y: 10)
     }
 }
 
