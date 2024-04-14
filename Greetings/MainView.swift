21fb786b77416cb1d24e005ecd19478022e8e753
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-14 09:40:05 -0700

    Renamed initial view to MainView and removed autogen helloworld

---
+++ Greetings/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  Greetings
+//
+//  Created by Guy Steinberg on 13/04/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
