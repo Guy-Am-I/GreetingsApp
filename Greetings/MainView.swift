47fb09d6f5eb7eb18f3672ca4553ca03ae979a99
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-14 10:24:02 -0700

    added tap gesture to change color

--- Greetings/MainView.swift
+++ Greetings/MainView.swift
@@ -31,7 +31,10 @@ struct MainView: View {
 
 struct TextView: View {
     let text: String
-    let color: Color
+    @State var color: Color
+    
+    let colors: [Color] = [
+        .red, .green, .black, .white, .yellow, .orange]
     
     var body: some View {
         Text(text)
@@ -42,6 +45,10 @@ struct TextView: View {
             .background(color.opacity(0.8))
             .clipShape(Capsule())
             .shadow(color: color.opacity(0.4), radius: 5, x: 10, y: 10)
+            .onTapGesture {
+                let len = colors.count
+                color = colors[Int.random(in: 0..<len)]
+            }
     }
 }
 
